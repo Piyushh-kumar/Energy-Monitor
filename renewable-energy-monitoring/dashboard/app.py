b1d2b046b671a5016686de8d954d119ad68bed90
code-eater-piyush <kpiyush032005@gmail.com>
2026-06-01 13:05:16 +0530

    p toP in path

--- renewable-energy-monitoring/dashboard/app.py
+++ renewable-energy-monitoring/dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from pathlib import path
+from pathlib import Path
 
 
 # ==========================================
